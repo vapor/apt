56f633c9c1f079dc9d5f6d92e6e8a2f89f17f9c3
Jonas Schwartz <jonas@qutheory.io>
2018-01-06 00:21:55 +0100

    Try to fix build script

--- 16.04/swift_3.1.0/build.sh
+++ 16.04/swift_3.1.0/build.sh
@@ -2,11 +2,11 @@
 
 cd src/
 
-curl -O https://swift.org/builds/swift-3.1-release/ubuntu1604/swift-3.1-RELEASE/swift-3.1-RELEASE-ubuntu16.04.tar.gz
+wget https://swift.org/builds/swift-3.1-release/ubuntu1604/swift-3.1-RELEASE/swift-3.1-RELEASE-ubuntu16.04.tar.gz
 
 tar zxvf swift-3.1-RELEASE-ubuntu16.04.tar.gz
 
-mv swift-3.1-RELEASE-ubuntu16.04/* .
+cp -R swift-3.1-RELEASE-ubuntu16.04/* .
 
 rm swift-3.1-RELEASE-ubuntu16.04.tar.gz
 rm -fr swift-3.1-RELEASE-ubuntu16.04
