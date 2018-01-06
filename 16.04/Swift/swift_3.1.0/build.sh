903c89275d3556c443d3e4aed37f81cf6cbe6b87
Jonas Schwartz <jonas@qutheory.io>
2018-01-06 22:39:58 +0100

    Cleaning up output

--- 16.04/Swift/swift_3.1.0/build.sh
+++ 16.04/Swift/swift_3.1.0/build.sh
@@ -7,13 +7,11 @@ UBUNTU_VERSION2="ubuntu1604"
 
 cd src/
 
-echo "https://swift.org/builds/swift-$SWIFT_VERSION/$UBUNTU_VERSION2/swift-$SWIFT_VERSION2/swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz"
+curl -O https://swift.org/builds/swift-$SWIFT_VERSION/$UBUNTU_VERSION2/swift-$SWIFT_VERSION2/swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz 2>&1 /dev/null
 
-curl -O https://swift.org/builds/swift-$SWIFT_VERSION/$UBUNTU_VERSION2/swift-$SWIFT_VERSION2/swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz
+tar zxvf swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz 2>&1 /dev/null
 
-tar zxvf swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz
+cp -R swift-$SWIFT_VERSION2-$UBUNTU_VERSION/* . 2>&1 /dev/null
 
-cp -R swift-$SWIFT_VERSION2-$UBUNTU_VERSION/* .
-
-rm swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz
-rm -fr swift-$SWIFT_VERSION2-$UBUNTU_VERSION
+rm swift-$SWIFT_VERSION2-$UBUNTU_VERSION.tar.gz 2>&1 /dev/null
+rm -fr swift-$SWIFT_VERSION2-$UBUNTU_VERSION 2>&1 /dev/null
