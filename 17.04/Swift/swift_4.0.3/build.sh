d39ebbe03a51663528895024b6d80339960cbb5d
Jonas Schwartz <jonas@qutheory.io>
2018-01-07 16:09:00 +0100

    Fixing Ubuntu 17.04

--- 17.04/Swift/swift_4.0.3/build.sh
+++ 17.04/Swift/swift_4.0.3/build.sh
@@ -2,8 +2,8 @@
 
 SWIFT_VERSION="4.0.3-release"
 SWIFT_VERSION2="$(tr [a-z] [A-Z] <<< "$SWIFT_VERSION")"
-UBUNTU_VERSION="ubuntu17.04"
-UBUNTU_VERSION2="ubuntu1704"
+UBUNTU_VERSION="ubuntu16.10"
+UBUNTU_VERSION2="ubuntu1610"
 
 cd src/
 
