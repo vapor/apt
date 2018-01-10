cff3da10405168163b7cd0e6600b13eb34f3d05d
Steffen D. Sommer <steffendsommer@icloud.com>
2018-01-10 13:32:40 +0100

    WIP: Add CStack for 14.04

---
+++ 14.04/OtherPackages/cstack_1.0.0/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd src/
+
+curl -O https://github.com/nodes-vapor/stack/releases/download/0.0.0/stack-linux.tar.gz
+
+tar zxf stack-linux.tar.gz
+
+mv stack-linux/libstack.so usr/local/lib
+mv stack-linux/stack.h usr/local/include
+
+rm stack-linux.tar.gz
+rm -fr stack-linux
