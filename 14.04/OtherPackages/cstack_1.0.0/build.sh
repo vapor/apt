53707ab94aad5e797560009d8f7a06eeb70f51f2
Steffen D. Sommer <steffendsommer@icloud.com>
2018-01-10 15:31:46 +0100

    Add trailing /

--- 14.04/OtherPackages/cstack_1.0.0/build.sh
+++ 14.04/OtherPackages/cstack_1.0.0/build.sh
@@ -6,8 +6,8 @@ wget https://github.com/nodes-vapor/stack/releases/download/0.0.0/stack-linux.ta
 
 tar zxf stack-linux.tar.gz
 
-mv stack-linux/libstack.so usr/local/lib
-mv stack-linux/stack.h usr/local/include
+mv stack-linux/libstack.so usr/local/lib/
+mv stack-linux/stack.h usr/local/include/
 
 rm stack-linux.tar.gz
 rm -fr stack-linux
