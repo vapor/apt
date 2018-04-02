83b2f009ecb08558d97c6198771211b8a5fe92a4
Jonas Schwartz <jonas@qutheory.io>
2018-04-02 15:56:28 -0400

    Fixing correct path

--- 14.04/Swift/swift_4.1.0/build.sh
+++ 14.04/Swift/swift_4.1.0/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SWIFT_VERSION="4.1.0-release"
+SWIFT_VERSION="4.1-release"
 SWIFT_VERSION2="$(tr [a-z] [A-Z] <<< "$SWIFT_VERSION")"
 UBUNTU_VERSION="ubuntu14.04"
 UBUNTU_VERSION2="ubuntu1404"
